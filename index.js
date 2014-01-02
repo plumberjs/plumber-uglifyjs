39893d3883c6494ba3b0cb882ce980229c17b1a0
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-01-02 10:57:03 +0000

    Initial import

---
+++ index.js
@@ -0,0 +1,23 @@
+var mapEachResource = require('plumber').mapEachResource;
+
+var UglifyJS = require('uglify-js');
+
+
+module.exports = function(/* no options */) {
+    return mapEachResource(function(resource) {
+        // TODO: filename, based on output spec? on input filename?
+        var minResource = resource.withTransformation('minimised', 'min');
+        var mapResource = minResource.withExtension('map');
+
+        var sourceMapData = UglifyJS.SourceMap();
+        var result = UglifyJS.minify(resource.path().absolute(), {
+            outSourceMap: mapResource.filename(),
+            source_map: sourceMapData
+        });
+
+        return [
+            minResource.withData(result.code),
+            mapResource.withData(sourceMapData.toString())
+        ];
+    });
+};
