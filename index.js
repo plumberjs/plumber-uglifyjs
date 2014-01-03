a95d9cb9dfd4509e64ab5a3b2e572ba0d38e7b81
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-01-03 12:25:08 +0000

    Use custom API to actually get sourcemap out

--- index.js
+++ index.js
@@ -5,17 +5,34 @@ var UglifyJS = require('uglify-js');
 
 module.exports = function(/* no options */) {
     return mapEachResource(function(resource) {
-        // TODO: filename, based on output spec? on input filename?
         var minResource = resource.withTransformation('minimised', 'min');
 
-        var sourceMapData = UglifyJS.SourceMap();
-        var result = UglifyJS.minify(resource.path().absolute(), {
-            outSourceMap: minResource.sourceMapFilename(),
+        // Note: we use the more custom API so we can pass data in and
+        // get source maps out
+
+        var toplevel_ast = UglifyJS.parse(resource.data());
+        toplevel_ast.figure_out_scope();
+
+        // TODO: catch and report warnings?
+        var compressor = UglifyJS.Compressor({warnings: false});
+        var compressed_ast = toplevel_ast.transform(compressor);
+
+        compressed_ast.figure_out_scope();
+        compressed_ast.compute_char_frequency();
+        compressed_ast.mangle_names();
+
+        var sourceMapData = UglifyJS.SourceMap({
+            file: minResource.sourceMapFilename(),
+            // pass through current sourcemap if any
+            orig: resource.sourceMap()
+        });
+
+        var uglifiedData = compressed_ast.print_to_string({
             source_map: sourceMapData
         });
 
         return minResource.
-            withData(result.code).
+            withData(uglifiedData).
             withSourceMap(sourceMapData.toString());
     });
 };
