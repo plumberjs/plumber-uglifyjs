383892bfd87b0b23ceceab09fd317badd3ad1470
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-02-04 01:49:56 +0000

    Upgrade to use mercator source maps

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var mapEachResource = require('plumber').mapEachResource;
+var SourceMap = require('mercator').SourceMap;
 
 var UglifyJS = require('uglify-js');
 
@@ -24,18 +25,25 @@ module.exports = function(/* no options */) {
         compressed_ast.compute_char_frequency();
         compressed_ast.mangle_names();
 
-        var sourceMapData = UglifyJS.SourceMap({
+        var sourceMapHolder = UglifyJS.SourceMap({
             file: minResource.filename(),
             // pass through current sourcemap if any
             orig: resource.sourceMap()
         });
 
         var uglifiedData = compressed_ast.print_to_string({
-            source_map: sourceMapData
+            source_map: sourceMapHolder
         });
 
-        return minResource.
-            withData(uglifiedData).
-            withSourceMap(sourceMapData.toString());
+        var sourceMap = SourceMap.fromMapData(sourceMapHolder.toString());
+
+        // FIXME: using uglify's input sourcemap feature above, but it
+        // seems to lose sources content, names and columns.  This
+        // below, on the other hand, breaks other tests...
+        // if (resource.sourceMap()) {
+        //     sourceMap = resource.sourceMap().apply(sourceMap);
+        // }
+
+        return minResource.withData(uglifiedData, sourceMap);
     });
 };
